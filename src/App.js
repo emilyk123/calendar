2b8938102e47eba4fc2edaed257c53bd9ad18ac5
emilyk123 <emilykrohn@csu.fullerton.edu>
2022-11-07 10:51:55 -0800

    Added a home page and other pages
    There are links to other pages on the homepage. Moved the todo list out of App.js and into TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react'
-import TodoList from './TodoList';
-import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Home from './component/home';
+import About from './component/about';
+import Contact from './component/contact';
+import TodoListApp from './component/TodoListApp';
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
-
-function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] )
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
-
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <div className="App">
+          <ul className="App-header">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/TodoListApp">Todo List</Link>
+            </li>
+          </ul>
+          <Routes>
+                <Route exact path='/' element={< Home />}></Route>
+                <Route exact path='/about' element={< About />}></Route>
+                <Route exact path='/contact' element={< Contact />}></Route>
+                <Route exact path='/TodoListApp' element={< TodoListApp />}></Route>
+        </Routes>
+        </div>
+      </Router>
+    );
   }
-
-  function handleAddTodo(e) {
-      const name = todoNameRef.current.value
-      if (name === '') return
-      setTodos(prevTodos => {
-        return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
-      })
-      todoNameRef.current.value = null
-  }
-
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
-
-  return (
-    <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add</button>
-      <button onClick={handleClearTodos}> Clear Complete</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
-  )
 }
 
 export default App;
